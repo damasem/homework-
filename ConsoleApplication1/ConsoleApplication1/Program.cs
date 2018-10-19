8e0fe7d92013bd4410148f2fe2a49283abe42d9d
Damase <damase@mail.ru>
2018-10-19 20:54:50 +0300

    task 1,2

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите данные");
+            string userString = Console.ReadLine();
+            char[] c = userString.ToCharArray();
+            var g = c.GroupBy(i => i);
+            Console.WriteLine("count: " + g.Count());
+            foreach (var k in g)
+                Console.WriteLine(k.Key + " (" + k.Count() + ")");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task 2/Task 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите данные");
+            string userString = Console.ReadLine();
+            Console.WriteLine(Transliteration.Front(userString));
+            Console.ReadKey();
+        }
+    }
+}
